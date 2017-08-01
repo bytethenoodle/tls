f45bf3e08f52628b6ee880b97f9dbf04c3a86453
bytethenoodle <elbertjohnalejoorozco@gmail.com>
2017-08-02 03:54:59 +0800

    mov

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ let package = Package(
     name: "TLS",
     dependencies: [
         .Package(url: "https://github.com/bytethenoodle/core.git", majorVersion: 2),
-      	.Package(url: "https://github.com/bytethenoodle/sockets.git", majorVersion: 2),
-        .Package(url: "https://github.com/vapor/ctls.git", majorVersion: 1),
+      	.Package(url: "https://github.com/bytethenoodle/sockets.git", majorVersion: 2)//,
+        //.Package(url: "https://github.com/vapor/ctls.git", majorVersion: 1),
     ]
 )
