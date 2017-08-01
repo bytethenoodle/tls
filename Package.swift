c87791d4d3b0cd7c854af0574425cb9c8be844a8
bytethenoodle <elbertjohnalejoorozco@gmail.com>
2017-08-02 03:20:11 +0800

    updated deps

--- Package.swift
+++ Package.swift
@@ -3,8 +3,8 @@ import PackageDescription
 let package = Package(
     name: "TLS",
     dependencies: [
-        .Package(url: "https://github.com/vapor/core.git", majorVersion: 2),
-      	.Package(url: "https://github.com/vapor/sockets.git", majorVersion: 2),
+        .Package(url: "https://github.com/bytethenoodle/core.git", majorVersion: 2),
+      	.Package(url: "https://github.com/bytethenoodle/sockets.git", majorVersion: 2),
         .Package(url: "https://github.com/vapor/ctls.git", majorVersion: 1),
     ]
 )
